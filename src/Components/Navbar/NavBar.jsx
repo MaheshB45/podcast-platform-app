b4afd3f5f6eb75f9315232c5dd1dde6fbf244294
Mahesh Bahir <maheshbahir45@gmail.com>
2024-01-14 15:55:35 +0530

    Add and create Navbar component

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,14 +1,24 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
+    const location = useLocation();
+    const currentPath = location.pathname;
   return (
     <div className='NavBar'>
        <div className='gradient'></div>
-        <NavLink to={"/"}>Signup</NavLink>
-        <NavLink to={"/podcasts"}>Podcasts</NavLink>
-        <NavLink to={"/startApodcast"}>Start A Podcast</NavLink>
-        <NavLink to={"/profile"}>Profile</NavLink>
+        <NavLink to="/" className={currentPath == "/" ? "active" : ""} >
+         Signup
+        </NavLink>
+        <NavLink to="/podcasts" className={currentPath == "/podcasts" ? "active" : ""}>
+         Podcasts
+        </NavLink>
+        <NavLink to="/start-a-podcast" className={currentPath == "/start-a-podcast" ? "active" : ""}>
+         Start A Podcast
+        </NavLink>
+        <NavLink to="/profile" className={currentPath == "/profile" ? "active" : ""}>
+         Profile
+        </NavLink>
     </div>
   )
 }
