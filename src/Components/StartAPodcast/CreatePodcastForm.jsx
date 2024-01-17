4eb85d3132d76eef6da1c9293d4a3b1d4814a20a
Mahesh Bahir <maheshbahir45@gmail.com>
2024-01-17 18:13:01 +0530

    Uploading Files in Firebase Storage

--- src/Components/StartAPodcast/CreatePodcastForm.jsx
+++ src/Components/StartAPodcast/CreatePodcastForm.jsx
@@ -5,6 +5,9 @@ import InputComponent from "../Common Components/Input/Input";
 import FileInput from "../Common Components/Input/FileInput";
 import Button from "../Common Components/Button/Button";
 import { toast } from "react-toastify";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { auth, db, storage } from "../../firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 const CreatePodcastForm = () => {
   const [title, setTitle] = useState("");
@@ -12,17 +15,57 @@ const CreatePodcastForm = () => {
   const [displayImg, setDisplayImg] = useState();
   const [bannerImg, setBannerImg] = useState();
 
- // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   //const navigate = useNavigate();
   //const dispatch = useDispatch();
 
-  const handleSubmit = () => {
-    toast.success("Handling Form");
+  const handleSubmit = async () => {
+    
     if (title && desc && displayImg && bannerImg) {
+        setLoading(true);
         // 1. Upload files -> get downloadable links
+        try {
+            const bannerImageRef = ref(
+                storage,
+                `podcasts/${auth.currentUser.uid}/${Date.now()}`
+            );
+            await uploadBytes(bannerImageRef, bannerImg);
+    
+            const bannerImageUrl = await getDownloadURL(bannerImageRef);
+
+            const displayImageRef = ref(
+                storage,
+                `podcasts/${auth.currentUser.uid}/${Date.now()}`
+            );
+            await uploadBytes(displayImageRef, displayImg);
+    
+            const displayImageUrl = await getDownloadURL(displayImageRef);
+
+            const podcastData = {
+                title: title,
+                description: desc,
+                bannerImg: bannerImageUrl,
+                displayImg: displayImageUrl,
+                createdBy: auth.currentUser.uid,
+            };
+
+            await addDoc(collection(db, "podcasts"), podcastData);
+            setTitle("");
+            setDesc("");
+            setBannerImg(null);
+            setDisplayImg(null);
+
+            toast.success("Podcast Created!");
+            setLoading(false);
+        } catch (e){
+            toast.error(e.message);
+            console.log(e);
+            setLoading(false);
+        }
 
     } else {
-        toast.error("Please Enter All values")
+        toast.error("Please Enter All values");
+        setLoading(false);
     }
   };
 
@@ -66,7 +109,8 @@ const CreatePodcastForm = () => {
       />
 
       <Button
-        
+        text={loading ? "Loading..." : "Create Podcast"}
+        disabled={loading}
         onClick={handleSubmit}
       />
     </>
